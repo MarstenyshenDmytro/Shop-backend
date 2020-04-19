964288cc1b5889f238ddf487abf3f7d70949926a
MarstenyshenDmytro <31610911+MarstenyshenDmytro@users.noreply.github.com>
2020-04-19 17:07:30 +0300

    fix req

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,14 @@ var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
+router.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 router.get("/", function (req, res, next) {
   res.json({
     name: "Ivan",
