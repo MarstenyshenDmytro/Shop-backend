7833a310c3ac42052fa060f664e26f24466af640
MarstenyshenDmytro <31610911+MarstenyshenDmytro@users.noreply.github.com>
2020-04-19 16:19:11 +0300

    fix request

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  response.json({
+  res.json({
     name: "Ivan",
     password: "123",
   });
