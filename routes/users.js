f7a4cf0933295cc9b44e2c0bbb9ff5f84453d7b0
MarstenyshenDmytro <31610911+MarstenyshenDmytro@users.noreply.github.com>
2020-04-21 13:06:56 +0300

    fix

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,7 @@ client.connect();
 client.query(
   "SELECT table_schema, table_name FROM information_schema.tables;",
   (err, res) => {
-    if (err) console.log(err);
+    if (err) throw err;
     for (let row of res.rows) {
       console.log(JSON.stringify(row));
     }
