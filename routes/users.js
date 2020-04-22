b3221fc844be41a3baad46891c6216b1bc44ee73
MarstenyshenDmytro <31610911+MarstenyshenDmytro@users.noreply.github.com>
2020-04-23 01:41:39 +0300

    test DB

--- routes/users.js
+++ routes/users.js
@@ -9,21 +9,6 @@ const client = new Client({
 });
 console.log(process.env.DATABASE_URL);
 
-client.connect((err) => {
-  if (err) {
-    console.error("connection error", err.stack);
-  } else {
-    console.log("connected");
-  }
-});
-
-client.query("SELECT 1", (err, res) => {
-  if (err) console.log(err);
-
-  console.log(res);
-
-  client.end();
-});
 /* GET users listing. */
 router.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
@@ -34,9 +19,24 @@ router.use(function (req, res, next) {
   next();
 });
 router.get("/", function (req, res, next) {
-  res.json({
-    name: "Ivan",
-    password: "123",
+  client.connect((err) => {
+    if (err) {
+      console.error("connection error", err.stack);
+    } else {
+      console.log("connected");
+    }
+  });
+
+  client.query("SELECT * FROM products WHERE id=1", (err, dbRes) => {
+    if (err) console.log(err);
+
+    console.log(dbRes);
+
+    res.json({
+      data: dbRes,
+    });
+
+    client.end();
   });
 });
 
