cf46181a1e51a37bde38bb0a30ec8014c1f3e8ce
MarstenyshenDmytro <31610911+MarstenyshenDmytro@users.noreply.github.com>
2020-04-23 11:12:42 +0300

    fix

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var router = express.Router();
 
-const getAllProducts = require("../queries/allProducts");
-
 /* GET users listing. */
 router.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
@@ -13,6 +11,7 @@ router.use(function (req, res, next) {
   next();
 });
 router.get("/", function (req, res, next) {
+  const getAllProducts = require("../queries/allProducts");
   getAllProducts();
   // const { Client } = require("pg");
 
