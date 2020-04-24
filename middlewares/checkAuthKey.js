0c2084a799a0d0c60b6bffa8da324cd78236de75
MarstenyshenDmytro <31610911+MarstenyshenDmytro@users.noreply.github.com>
2020-04-24 19:59:43 +0300

    fix

--- middlewares/checkAuthKey.js
+++ middlewares/checkAuthKey.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   const key = req.headers["x-access-token"] || req.headers["authorization"];
   //if no token found, return response (without going to the next middelware)
-  if (!key) return res.status(401).send("Access denied. No token provided.");
+  if (!key) return res.status(401).header("Status Code", 401);
 
   try {
     //if can verify the token, set req.user and pass to next middleware
@@ -15,6 +15,6 @@ module.exports = (req, res, next) => {
     next();
   } catch (ex) {
     //if invalid token
-    res.status(400).send("Invalid token.");
+    res.status(400).header("Status Code", 400);
   }
 };
