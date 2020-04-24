1aa15d879a6f375f4b7fd2406de539c43b2f7480
MarstenyshenDmytro <31610911+MarstenyshenDmytro@users.noreply.github.com>
2020-04-24 20:06:23 +0300

    fix

--- middlewares/checkAuthKey.js
+++ middlewares/checkAuthKey.js
@@ -3,18 +3,25 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   const key = req.headers["x-access-token"] || req.headers["authorization"];
-  //if no token found, return response (without going to the next middelware)
-  if (!key) return res.send(401, "Access denied. No token provided.");
-
-  try {
-    //if can verify the token, set req.user and pass to next middleware
-    const decoded = jwt.verify(key, authKey);
-
-    console.log(decoded);
-    //req.user = decoded;
+  if (key === authKey) {
+    console.log(1);
     next();
-  } catch (ex) {
-    //if invalid token
-    res.send(400, "Invalid token.");
+  } else {
+    res.send(400, "Bad authorization key.");
   }
+  //   const key = req.headers["x-access-token"] || req.headers["authorization"];
+  //   //if no token found, return response (without going to the next middelware)
+  //   if (!key) return res.send(401, "Access denied. No token provided.");
+
+  //   try {
+  //     //if can verify the token, set req.user and pass to next middleware
+  //     const decoded = jwt.verify(key, authKey);
+
+  //     console.log(decoded);
+  //     //req.user = decoded;
+  //     next();
+  //   } catch (ex) {
+  //     //if invalid token
+  //     res.send(400, "Invalid token.");
+  //   }
 };
