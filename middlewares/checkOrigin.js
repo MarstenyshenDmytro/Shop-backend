6c25c00180149b2fd65e39720d1f6c844a3e7055
MarstenyshenDmytro <31610911+MarstenyshenDmytro@users.noreply.github.com>
2020-04-24 21:29:16 +0300

    fix

--- middlewares/checkOrigin.js
+++ middlewares/checkOrigin.js
@@ -3,13 +3,13 @@ const authKey = require("../constants");
 module.exports = (req, res, next) => {
   console.log(req.headers["authorization"]);
   console.log(req.headers.origin);
-  if (req.headers["authorization"] === authKey) {
-    console.log(1);
-    res.header("Access-Control-Allow-Origin", req.headers.origin); // update to match the domain you will make the request from
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, Authorization, X-Requested-With, Content-Type, Accept"
-    );
-  }
+  // if (req.headers["authorization"] === authKey) {
+  console.log(1);
+  res.header("Access-Control-Allow-Origin", req.headers.origin); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, Authorization, X-Requested-With, Content-Type, Accept"
+  );
+  //}
   next();
 };
