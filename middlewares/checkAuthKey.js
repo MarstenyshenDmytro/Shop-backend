38f9156ac8dfe3cc8f4ad47f9cde7146808bc356
MarstenyshenDmytro <31610911+MarstenyshenDmytro@users.noreply.github.com>
2020-04-24 19:39:38 +0300

    fix

--- middlewares/checkAuthKey.js
+++ middlewares/checkAuthKey.js
@@ -4,8 +4,7 @@ module.exports = (req, res, next) => {
   const key = req.headers["x-access-token"] || req.headers["authorization"];
   if (key === authKey) {
     console.log(1);
-    next();
-  } else {
-    res.status(401).send("Bad authorization key.");
+    return next();
   }
+  res.status(401).send("Bad authorization key.");
 };
