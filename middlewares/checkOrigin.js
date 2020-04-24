45d24e8d40045b3c0ac1c4219111d6578d9b4502
MarstenyshenDmytro <31610911+MarstenyshenDmytro@users.noreply.github.com>
2020-04-24 19:04:22 +0300

    fix

--- middlewares/checkOrigin.js
+++ middlewares/checkOrigin.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", origin); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", req.headers.origin); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, Authorization, X-Requested-With, Content-Type, Accept"
