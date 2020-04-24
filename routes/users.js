3771a6e4eca4c6672923070afd4c5c2a2197d1fa
MarstenyshenDmytro <31610911+MarstenyshenDmytro@users.noreply.github.com>
2020-04-24 19:02:25 +0300

    fix

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,8 @@ var router = express.Router();
 const pgClient = require("../pgClient/client");
 
 /* GET users listing. */
-router.use(require("../middlewares/checkOrigin"););
-router.use(require("../middlewares/checkAuthKey"););
+router.use(require("../middlewares/checkOrigin"));
+router.use(require("../middlewares/checkAuthKey"));
 router.get("/", function (req, res, next) {
   const client = pgClient();
 
