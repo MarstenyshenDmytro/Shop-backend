0932e2d0de834a3ae2ad3eb486c83b9f93565bbc
MarstenyshenDmytro <31610911+MarstenyshenDmytro@users.noreply.github.com>
2020-04-24 18:49:26 +0300

    fix

--- middlewares/checkAuthKey.js
+++ middlewares/checkAuthKey.js
@@ -2,10 +2,11 @@ const authKey = require("../constants");
 
 const checkAuthKey = (req, res, next) => {
   const { authorization } = req.headers;
+  console.log(authorization);
   if (authorization === authKey) {
     next();
   } else {
-    return res.status(401).send("Bad authorization key.");
+    res.status(401).send("Bad authorization key.");
   }
 };
 
