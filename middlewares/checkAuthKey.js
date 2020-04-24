0e2e23253acb9513a0ccbcfa246b1ff37ff5ab36
MarstenyshenDmytro <31610911+MarstenyshenDmytro@users.noreply.github.com>
2020-04-24 19:25:14 +0300

    fix

--- middlewares/checkAuthKey.js
+++ middlewares/checkAuthKey.js
@@ -2,7 +2,9 @@ const authKey = require("../constants");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
+  consoloe.log(authorization);
   if (authorization === authKey) {
+    console.log(1);
     next();
   } else {
     res.status(401).send("Bad authorization key.");
--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,10 @@
 var express = require("express");
 var router = express.Router();
 const pgClient = require("../pgClient/client");
-const authKey = require("../constants");
 
 /* GET users listing. */
-router.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.headers.origin); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, Authorization, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-router.use(function (req, res, next) {
-  const { authorization } = req.headers;
-  if (authorization === authKey) {
-    next();
-  } else {
-    res.status(401).send("Bad authorization key.");
-  }
-});
+router.use(require("../middlewares/checkOrigin"));
+router.use(require("../middlewares/checkAuthKey"));
 router.get("/", function (req, res, next) {
   const client = pgClient();
 
