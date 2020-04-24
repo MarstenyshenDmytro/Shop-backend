c6b60be2988366fe8c98630c3eb6d433c7449fe8
MarstenyshenDmytro <31610911+MarstenyshenDmytro@users.noreply.github.com>
2020-04-24 19:42:50 +0300

    fix

--- middlewares/checkOrigin.js
+++ middlewares/checkOrigin.js
@@ -5,5 +5,11 @@ module.exports = async (req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, Authorization, X-Requested-With, Content-Type, Accept"
   );
-  next();
+  const key = req.headers["x-access-token"] || req.headers["authorization"];
+  if (key === authKey) {
+    console.log(1);
+    next();
+  } else {
+    res.status(401).send("Bad authorization key.");
+  }
 };
