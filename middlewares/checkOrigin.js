bf3ff92c85ea2990daeae08d79428ac7aa6b1118
MarstenyshenDmytro <31610911+MarstenyshenDmytro@users.noreply.github.com>
2020-04-25 14:25:46 +0300

    fi

--- middlewares/checkOrigin.js
+++ middlewares/checkOrigin.js
@@ -1,18 +1,10 @@
 const authKey = require("../constants");
 
 module.exports = (req, res, next) => {
-  console.log("Origin");
-  console.log(req.headers["authorization"]);
-  console.log(req.headers.origin);
-  // if (req.headers["authorization"] === authKey) {
-  console.log(1);
-
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, Authorization, X-Requested-With, Content-Type, Accept"
   );
-  res.header("WWW-Authenticate", `Basic realm="Access to the standing site"`);
-  //}
   next();
 };
