fb02e6770026457917338cfe987aa5d0da51a429
MarstenyshenDmytro <31610911+MarstenyshenDmytro@users.noreply.github.com>
2020-04-30 16:58:06 +0300

    add filter

--- routes/products.js
+++ routes/products.js
@@ -5,22 +5,39 @@ const pgClient = require("../pgClient/client");
 /* GET users listing. */
 //router.use(require("../middlewares/checkAuthKey"));
 //router.use(require("../middlewares/checkOrigin"));
+function filterQueryString(obj) {
+  let filterString;
+  Object.entries(obj).forEach(([key, value]) => {
+    if (value !== "none") {
+      filterString += `${key}=${value} and `;
+    }
+  });
+  if (filterString.length !== 0) {
+    return `WHERE ${filterString.slice(0, -5)}`;
+  }
+  return "";
+}
+
 router.get("/", function (req, res, next) {
   console.log(req.params);
   console.log(req.query);
   console.log(req.query.brand);
   console.log(typeof req.query);
+
   const client = pgClient();
   client.connect();
-  client.query("SELECT * FROM products ORDER BY id DESC", (err, dbRes) => {
-    if (err) console.log(err);
+  client.query(
+    `SELECT * FROM products ${filterQueryString(req.query)} ORDER BY id DESC`,
+    (err, dbRes) => {
+      if (err) console.log(err);
 
-    res.json({
-      data: dbRes.rows,
-    });
+      res.json({
+        data: dbRes.rows,
+      });
 
-    client.end();
-  });
+      client.end();
+    }
+  );
 });
 
 router.get("/:productId", function (req, res, next) {
