79786391b6f837be056ad67220f44deaf9a697d2
MarstenyshenDmytro <31610911+MarstenyshenDmytro@users.noreply.github.com>
2020-04-30 17:24:08 +0300

    fix

--- routes/products.js
+++ routes/products.js
@@ -2,9 +2,6 @@ var express = require("express");
 var router = express.Router();
 const pgClient = require("../pgClient/client");
 
-/* GET users listing. */
-//router.use(require("../middlewares/checkAuthKey"));
-//router.use(require("../middlewares/checkOrigin"));
 function filterQueryString(obj) {
   let filterString = "";
   Object.entries(obj).forEach(([key, value]) => {
